56df454d08dede1755f66d4c7133530d51efc847
Nicholas Dockery <113644239+NJDockery@users.noreply.github.com>
2022-10-16 18:06:07 +0000

    fixed validation

--- script.js
+++ script.js
@@ -2,22 +2,15 @@ function validateForm(){
   let x = document.getElementById("password").value;
   let y = document.getElementById("passwordConfirm").value;
   let result = x.localeCompare(y);
-  let a = usernames[0].toString();
-  let b = usernames[1].toString();
-  let c = usernames[2].toString();
-let e = document.getElementById("username").value;
+  let e = document.getElementById("username").value;
 if(result){
     alert("passwords do not match!");
-    return false;
-  if(a.localCompare(e)||b.localCompare(e)||c.localCompare(e)){
+    return false;}
+else if(usernames.includes(e)){
     alert("Username is already in use, please try another");
-    return false;
+    return false;}
+else{
+  return true;}
   }
-}
-  }
-}
 
 const usernames=["Harry", "Elizabeth", "Shana"];
-
-function validateUsername(){
-  
